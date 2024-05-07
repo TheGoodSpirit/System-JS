28ffb6dfdbbfdb5a5da3c1289f4b3be7926c1a8e
thegoodspirit <mannish079@gmail.com>
2024-05-07 12:51:54 +0545

    Day 2 Completed

---
+++ script/app.js
@@ -0,0 +1,35 @@
+// Task 1
+let _college = "Butwal Kalika Campus"
+let _course = "BCA"
+let _student = "Manish Ghimire"
+
+console.log(`
+College : ${_college},
+Course : ${_course},
+Name : ${_student}`
+);
+
+console.log(typeof _course)
+console.log(typeof true)
+
+let is = 19 >= 18
+console.log(is)
+
+let num1 = 22
+let num2 = num1 
+console.log(num2)
+
+num1 = 10
+console.log(num2)
+
+let _noData;
+console.log(_noData) // undifined dynamic datatypes.
+console.log(typeof null) // object
+console.log(typeof NaN) // number
+console.log(_college + ", " + _student) // add two string
+
+
+// vars, let, const
+console.log(_model)
+var _model = "Ford"
+// _model = "Duster"
