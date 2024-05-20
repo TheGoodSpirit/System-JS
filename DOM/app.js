2d7ee1f614fc21b153a2daf363467396f9ff1846
thegoodspirit <mannish079@gmail.com>
2024-05-20 11:19:47 +0545

    store array and objects on localStorage

--- DOM/app.js
+++ DOM/app.js
@@ -1,4 +1,4 @@
-let heading = document.getElementById('head')
+// let heading = document.getElementById('head')
 // console.log(heading)
 
 // let heading1 = document.getElementsByClassName('head1')
@@ -10,13 +10,13 @@ let heading = document.getElementById('head')
 //     console.log(head)
 // })
 
-heading.innerHTML = "<span>sub ele </span>"
-heading.textContent = "<span>sub   yu ele </span>"
+// heading.innerHTML = "<span>sub ele </span>"
+// heading.textContent = "<span>sub   yu ele </span>"
 
-console.log(heading.textContent)
-console.log(heading.innerText)
+// console.log(heading.textContent)
+// console.log(heading.innerText)
 
-heading.classList.add('newClass') 
+// heading.classList.add('newClass') 
 // // Array of fruits
 // const fruits = ["apple", "banana", "watermelom", "mango", "pineapple"]
 
@@ -60,3 +60,47 @@ heading.classList.add('newClass')
 // })
 // document.write('</ol>')
 
+
+
+// store array in local storage
+
+
+// storage for user details
+const id = JSON.parse(localStorage.getItem("users"))[0];
+const name = JSON.parse(localStorage.getItem("users"))[1]; 
+const password = JSON.parse(localStorage.getItem("users"))[2];
+
+console.log(JSON.parse(localStorage.getItem("users"))[0].length)
+
+function saveData() {
+    // get user input
+    // const userId = document.getElementById("id").value ;
+    const userName = document.getElementById("name").value;
+    const userPassword = document.getElementById("password").value;
+
+    if (userName === "" || userPassword === ""){
+         alert("Please fill out all fields");
+    } else {
+        id.push(parseInt(JSON.parse(localStorage.getItem("users"))[0].length));
+        name.push(userName);
+        password.push(userPassword);
+        localStorage.setItem("users", JSON.stringify([ id, name, password]));
+    }
+}
+
+
+// store objects in localStorage
+// const User_Data = {
+//     id: 2,
+//     name: "John Doe",
+//     password: "qwertyuiop",
+//     email: "john.doe@gmail.com"
+//     }
+// localStorage.setItem("userInfo", JSON.stringify(User_Data))
+
+
+// function getUserDetails() {
+//     const userObj = JSON.parse(localStorage.getItem("userInfo"))
+//     console.log(userObj);
+// }
+// getUserDetails();
