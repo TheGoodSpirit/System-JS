7b016548157adef4d10c4ffc76f2a7f40270395a
thegoodspirit <mannish079@gmail.com>
2024-05-21 10:53:51 +0545

    Made Digital clock

--- Projects/Digital Clock/app.js
+++ Projects/Digital Clock/app.js
@@ -1,21 +1,35 @@
-let time = document.getElementById('time');
+let Hrs = document.getElementById('hrs');
+let Min = document.getElementById('min');
+let Sec = document.getElementById('sec');
+let Period = document.getElementById('period');
+
+
 let toggleBtn = document.getElementById('btn')
 let section  = document.getElementById('section')
-time.style.userSelect = "none";
 
 function currentTime() {
     let date = new Date();
 
-    let hrs = date.getHours();
+    let hrs = date.getHours()
     let min = date.getMinutes();    
     let sec = date.getSeconds();
 
-    if(hrs > 10 || min > 10 || sec >10) {
-        time.textContent = ` ${addZero(hrs)} : ${addZero(min)} : ${addZero(sec)}`;
+    let period = "AM";
+    if (hrs >= 12) {
+        period = "PM";
+        // hrs = hrs - 12;
     }
+
+    if(hrs > 10 || min > 10 || sec >10) {
+        Hrs.textContent = ` ${addZero(hrs)} `;
+        Min.textContent = ` ${addZero(min)} `;
+        Sec.textContent = ` ${addZero(sec)} `;
+        Period.textContent = `${period}`; 
+    }
+
 }
 
-setInterval(currentTime, 100);
+setInterval(currentTime, 1000);
 
 const  addZero =  (num) => {
     return num >= 10 ? num : `0${num}`
